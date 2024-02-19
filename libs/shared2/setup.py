e12fb9655a6e70f67a795ea9d1365297f664b8b6
thomas.phipps <thomas.phipps@kibocommerce.com>
2024-02-19 09:13:54 -0600

    meh

--- libs/shared2/setup.py
+++ libs/shared2/setup.py
@@ -6,7 +6,10 @@ setup(
     name="shared2",
     version="0.1.0",
     description="A sample Python package",
-    author="Your Name",       install_requires=[]
-
+    author="Your Name",       
+    package_dir={"": "src"},  # tell setuptools that packages are under 'src'
+    packages=find_packages(where="src"),  # find packages under 'src'
+    install_requires=[]
+    
    
 )
